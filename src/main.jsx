6ea2d8d90bc6117aa87ad222e282886917225211
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-04 18:44:31 +0600

    added newsconptext to root.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import MainRoute from './Routes/MainRoute'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import NewsProvider from './Components/Context/NewsProvider';
+import MainRoute from './Routes/MainRoute';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={MainRoute}></RouterProvider>
-  </React.StrictMode>,
-)
+    <NewsProvider>
+      <RouterProvider router={MainRoute}></RouterProvider>
+    </NewsProvider>
+  </React.StrictMode>
+);

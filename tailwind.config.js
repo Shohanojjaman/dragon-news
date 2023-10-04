c9a78e02b6960dd95fd25abb69477dff8704fc35
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-04 11:26:21 +0600

    Added some color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,16 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily:{
-        poppins:"'Poppins', sans-serif"
-      }
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
+      colors: {
+        heading: '#403F3F',
+        body: '#706F6F',
+        'light-body': '#9F9F9F',
+        theme: '#F75B5F',
+        button: '#D72050',
+      },
     },
   },
   plugins: [require('daisyui')],

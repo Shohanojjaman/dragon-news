ae2dd5a6f0f9bf9ebaa66284472e5974a09f8f68
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-04 18:38:19 +0600

    Created Navbar components.

---
+++ src/Components/Header/Navbar.jsx
@@ -0,0 +1,82 @@
+import { Link, NavLink } from 'react-router-dom';
+
+const defaultUserPic = 'https://i.ibb.co/zxqmRnL/user.png';
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink
+          to={'/'}
+          className={`${({ isActive, isPending }) =>
+            isPending ? 'pending' : isActive ? 'text-heading bg-none' : ''}} text-body text-xl bg-none`}>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={'/about'}
+          className={`${({ isActive, isPending }) =>
+            isPending ? 'pending' : isActive ? 'text-heading bg-none' : ''}} text-body text-xl bg-none`}>
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={'/career'}
+          className={`${({ isActive, isPending }) =>
+            isPending ? 'pending' : isActive ? 'text-heading bg-none' : ''}} text-body text-xl bg-none`}>
+          Career
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            {links}
+          </ul>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end flex gap-2">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img className="w-10" src={defaultUserPic} alt="" />
+            </div>
+          </label>
+          <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+            <li>
+              <Link>Profile</Link>
+            </li>
+            <li>
+              <Link>Settings</Link>
+            </li>
+            <li>
+              <Link>Logout</Link>
+            </li>
+          </ul>
+        </div>
+        <Link className="bg-[#403F3F] text-white px-10 py-2 font-semibold text-xl" to={'/login'}>
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

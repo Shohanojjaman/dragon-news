c435f294a8b6b2a3b72033323ac5a7704d286729
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-07 13:02:30 +0600

    Created QZone component.

---
+++ src/Components/QZone.jsx
@@ -0,0 +1,14 @@
+const QZone = () => {
+  return (
+    <div className="space-y-6 p-4 bg-[#F3F3F3]">
+      <h4 className="text-xl text-heading font-semibold">Q-Zone</h4>
+      <div className="space-y-8">
+        <img src="https://i.ibb.co/hYNf10L/qZone1.png" alt="" />
+        <img src="https://i.ibb.co/ThTn8wJ/qZone2.png" alt="" />
+        <img src="https://i.ibb.co/gPMqy9F/qZone3.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default QZone;

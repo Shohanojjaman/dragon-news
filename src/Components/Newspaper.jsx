db432271604ee39a018c691ef779ba39f677189b
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-07 13:02:55 +0600

    Created Newspaper component.

---
+++ src/Components/Newspaper.jsx
@@ -0,0 +1,13 @@
+const Newspaper = () => {
+  return (
+    <div className="newspaper text-white text-center space-y-6">
+      <h4 className="text-3xl font-bold">Create an Amazing Newspaper</h4>
+      <p className="text">
+        Discover thousands of options, easy to customize layouts, one-click to import demo and much more.
+      </p>
+      <button className="p-4 bg-button font-bold text-xl">Learn More</button>
+    </div>
+  );
+};
+
+export default Newspaper;
